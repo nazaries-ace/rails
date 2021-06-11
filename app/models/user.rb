9496d4dd21167938b104a3b0f9f0e14e18cc5b2e
nazaries <nazaries@nb0010ace.local>
2021-06-11 12:30:55 +0200

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-    
+    VALID_EMAIL_REGEX =  /\A(?=[A-Z0-9][A-Z0-9@._%+-]{5,253}+$)[A-Z0-9._%+-]{1,64}+@(?:(?=[A-Z0-9-]{1,63}+\.)[A-Z0-9]++(?:-[A-Z0-9]++)*+\.){1,8}+[A-Z]{2,63}+\z/i
+
+    validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 3, maximum: 25 }
+    validates :email, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 6, maximum: 50 },
+                format: { with: VALID_EMAIL_REGEX }
 end
