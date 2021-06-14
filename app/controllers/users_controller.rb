4d6a5a36aafa8c644ce784000945ede2f8cf5dc9
nazaries <nazaries@nb0010ace.local>
2021-06-14 11:42:04 +0200

    gravatar running

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,10 @@ class UsersController < ApplicationController
         else
             render 'new'
         end
+        
+    end
+    def show
+        @user = User.find(params[:id])
 
     end
 
